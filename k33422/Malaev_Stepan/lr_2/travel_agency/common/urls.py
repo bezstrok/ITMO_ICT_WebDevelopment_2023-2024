757adcbfcab8fb4629f0851f437fa58dbb6075fc
Malaev_Stepan <pocandre87@gmail.com>
2023-12-29 06:58:51 +0300

    add common app

---
+++ k33422/Malaev_Stepan/lr_2/travel_agency/common/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.IndexView.as_view(), name='index')
+]
+
+# Apps urls
+urlpatterns += [
+
+]
--- k33422/Malaev_Stepan/lr_2/travel_agency/config/settings.py
+++ k33422/Malaev_Stepan/lr_2/travel_agency/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS += [
 # Apps
 
 INSTALLED_APPS += [
+    'common.apps.CommonConfig',
     'users.apps.UsersConfig',
     'tours.apps.ToursConfig'
 ]
@@ -62,7 +63,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
