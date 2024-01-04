4730e9879abcf5133f0847469558d145c66027cb
Malaev_Stepan <pocandre87@gmail.com>
2024-01-04 12:47:13 +0300

    Implement explore page

--- k33422/Malaev_Stepan/lr_2/travel_agency/tours/views.py
+++ k33422/Malaev_Stepan/lr_2/travel_agency/tours/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import ListView, RedirectView
 
-# Create your views here.
+from .models import Tour
+
+
+class TourListView(ListView):
+	model = Tour
+	paginate_by = 5
+	template_name = 'tours/explore.html'
+	
+	def get_queryset(self):
+		queryset = super().get_queryset().filter(start_date__gt=timezone.localdate())
+		
+		search_query = self.request.GET.get('search', '')
+		
+		if search_query:
+			queryset = queryset.filter(
+				Q(name__icontains=search_query) |
+				Q(country__icontains=search_query) |
+				Q(description__icontains=search_query)
+			)
+		
+		queryset = queryset.order_by('country')
+		
+		return queryset
+
+
+class TourDetailView(RedirectView):
+	url = reverse_lazy('explore')
