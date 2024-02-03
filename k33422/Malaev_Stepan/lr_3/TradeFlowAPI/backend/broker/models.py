5dae37b14e161df6dc51cbd5ba090a79e0424822
Malaev_Stepan <pocandre87@gmail.com>
2024-02-03 12:06:47 +0300

    minor fix models

--- k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/broker/models.py
+++ k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/broker/models.py
@@ -2,12 +2,10 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from backend.utils.models import AbstractTimeStampedModel
-
 User = settings.AUTH_USER_MODEL
 
 
-class Firm(AbstractTimeStampedModel):
+class Firm(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
     
@@ -19,7 +17,7 @@ class Firm(AbstractTimeStampedModel):
         return self.brokers.all().count()
 
 
-class Broker(AbstractTimeStampedModel):
+class Broker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="broker")
     profit_percentage = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
     fixed_monthly_amount = models.DecimalField(max_digits=10, decimal_places=2)
--- k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/manufacturer/models.py
+++ k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/manufacturer/models.py
@@ -1,12 +1,10 @@
 from django.conf import settings
 from django.db import models
 
-from backend.utils.models import AbstractTimeStampedModel
-
 User = settings.AUTH_USER_MODEL
 
 
-class Manufacturer(AbstractTimeStampedModel):
+class Manufacturer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.TextField()
     contact_info = models.TextField()
