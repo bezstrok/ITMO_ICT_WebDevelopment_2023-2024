6321740d2e69fe661058bc88c4f13b375332f12e
Malaev_Stepan <pocandre87@gmail.com>
2024-02-13 17:32:48 +0300

    implement brokers filters

--- k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/api/v1/broker/views.py
+++ k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/api/v1/broker/views.py
@@ -1,7 +1,10 @@
 from django.utils.translation import gettext_lazy as _
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions
 from rest_framework.exceptions import ValidationError
+from rest_framework.filters import OrderingFilter, SearchFilter
 
+from backend.api.v1.broker.filters import BrokerFilter
 from backend.api.v1.broker.serializers import (
     CRUDBrokerSerializer,
     ListBrokersSerializer,
@@ -15,6 +18,7 @@ from backend.broker.models import Broker
 
 class BrokerViewSet(ActionMeMixin, SpecificModelViewSet):
     queryset = Broker.objects.select_related('user', 'firm').all()
+    
     serializer_class = CRUDBrokerSerializer
     serializer_classes_by_action = {
         'retrieve': RetrieveBrokerSerializer,
@@ -22,6 +26,11 @@ class BrokerViewSet(ActionMeMixin, SpecificModelViewSet):
     }
     
     permission_classes = [permissions.IsAuthenticated, IsUserRoleObject]
+    
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
+    filterset_class = BrokerFilter
+    ordering = ('fixed_monthly_amount', 'profit_percentage')
+    search_fields = ('user__email',)
     
     def perform_create(self, serializer):
         if self.queryset.filter(user=self.request.user).exists():
--- k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/api/v1/firm/views.py
+++ k33422/Malaev_Stepan/lr_3/TradeFlowAPI/backend/api/v1/firm/views.py
@@ -7,13 +7,17 @@ from .serializers import FirmDetailSerializer, FirmListSerializer
 
 class FirmDetailView(generics.RetrieveAPIView):
     queryset = Firm.objects.prefetch_related('brokers').all()
+    
     serializer_class = FirmDetailSerializer
+    
     permission_classes = [permissions.IsAuthenticated]
 
 
 class FirmListView(generics.ListAPIView):
     queryset = Firm.objects.all()
+    
     serializer_class = FirmListSerializer
+    
     permission_classes = [permissions.IsAuthenticated]
     
     filter_backends = (OrderingFilter, SearchFilter)
